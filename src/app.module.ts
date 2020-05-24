04c16d4910c5801941bdc3afd650e713d25b978f
Luan Nguyen Trong <luannguyentrong98@gmail.com>
2020-05-24 11:08:53 +0700

    clean code part 1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,44 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AreaModule } from './area/area.module';
+import { join } from 'path';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { AreaModule } from './area/area.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { JobModule } from './job/job.module';
-import {ConfigModule} from '@nestjs/config';
 import { SharedModule } from './shared/shared.module';
 import { TeacherModule } from './teacher/teacher.module';
 import { CourseModule } from './course/course.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 import { NotificationModule } from './notification/notification.module';
 import { DeviceModule } from './device/device.module';
 import { SearchModule } from './search/search.module';
 
 @Module({
-  imports: [ ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGO_URI,{useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true,useFindAndModify: false}), 
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'public'),
-    }),
-    AreaModule, UserModule, AuthModule, JobModule, SharedModule,SharedModule, 
-    TeacherModule, CourseModule, NotificationModule, DeviceModule, SearchModule],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot(),
+		MongooseModule.forRoot(
+			process.env.MONGO_URI,
+			{
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+				useCreateIndex: true,
+				useFindAndModify: false
+			}
+		), 
+		ServeStaticModule.forRoot({
+			rootPath: join(__dirname, '..', 'public'),
+		}),
+		AreaModule,
+		UserModule,
+		AuthModule,
+		JobModule,
+		SharedModule, 
+		TeacherModule,
+		CourseModule,
+		NotificationModule,
+		DeviceModule,
+		SearchModule
+	]
 })
 export class AppModule {}
