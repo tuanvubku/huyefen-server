49cb509b8a4fe92fa660b41343380e8a07ff1c50
Luan Nguyen Trong <luannguyentrong98@gmail.com>
2020-06-06 14:54:37 +0700

    https server

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import { ConfigService } from '@nestjs/config';
 
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const httpsOptions = {
+    key: readFileSync(`${join(__dirname, '../', 'ssl')}/key.pem`),
+    cert: readFileSync(`${join(__dirname, '../', 'ssl')}/cert.pem`)
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get<string>('HTTPS_PORT'));
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
