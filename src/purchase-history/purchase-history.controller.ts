120c9d3c363251fd06b94317b461c916616efba5
Luan Nguyen Trong <luannguyentrong98@gmail.com>
2020-08-02 12:25:23 +0700

    purchase history

--- src/purchase-history/purchase-history.controller.ts
+++ src/purchase-history/purchase-history.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, ParseIntPipe, Query, UseGuards } from '@nestjs/common';
+import { PurchaseHistoryService } from '@/purchase-history/purchase-history.service';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from '@/utils/guards/roles.guard';
+import { Roles } from '@/utils/decorators/roles.decorator';
+import { IResponse } from '@/utils/interfaces/response.interface';
+import { Role } from '@/config/constants';
+import { User } from '@/utils/decorators/user.decorator';
+import { ResponseSuccess } from '@/utils/utils';
 
-@Controller('purchase-history')
-export class PurchaseHistoryController {}
+@Controller('api/purchase-history')
+export class PurchaseHistoryController {
+  constructor(
+    private readonly purchaseHistoryService: PurchaseHistoryService
+  ) {}
+
+  @Get()
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles(Role.User)
+  async fetchPurchaseHistory(
+    @User() user,
+    @Query('skip', ParseIntPipe) skip: number,
+    @Query('limit', ParseIntPipe) limit: number
+  ): Promise<IResponse<any>> {
+    const userId: string = user._id;
+    const result = await this.purchaseHistoryService.fetchPurchaseHistory(userId, skip, limit);
+    return new ResponseSuccess('FETCH_OK', result);
+  }
+}
--- src/purchase-history/purchase-history.service.ts
+++ src/purchase-history/purchase-history.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { IPurchaseHistory, IPurchaseItem } from '@/purchase-history/interfaces/purchase-history.interface';
-import { map, capitalize } from 'lodash';
+import { map, capitalize, pick, slice } from 'lodash';
+import { mapKeyToPrice } from '@/utils/utils';
 
 @Injectable()
 export class PurchaseHistoryService {
@@ -23,4 +24,32 @@ export class PurchaseHistoryService {
     });
     await purchaseHistory.save();
   }
+
+  async fetchPurchaseHistory(userId: string, skip: number, limit: number): Promise<any> {
+    const rawData = await this.purchaseHistoryModel
+      .find({ user: userId })
+      .populate('items.item', '_id title avatar price')
+      .sort('-createdAt')
+      .lean()
+      .exec()
+    const data = map(rawData, purchaseHistory => {
+      return {
+        _id: purchaseHistory._id,
+        paymentType: purchaseHistory.paymentWay,
+        date: purchaseHistory.createdAt,
+        totalPrice: purchaseHistory.price,
+        items: map(purchaseHistory.items, item => ({
+          //@ts-ignore
+          ...pick(item.item, ['_id', 'title', 'avatar']),
+          type: item.type,
+          price: mapKeyToPrice(item.item.price)
+        }))
+      };
+    })
+    return {
+      total: data.length,
+      list: slice(data, skip, skip + limit)
+    }
+  }
+
 }
